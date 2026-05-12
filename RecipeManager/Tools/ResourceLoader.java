8a5cd78af265a9edf48682772bfd510618692d0b
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-05-12 14:23:59 +0400

    Code Changes

--- RecipeManager/Tools/ResourceLoader.java
+++ RecipeManager/Tools/ResourceLoader.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ResourceLoader {
-	private BufferedImage image;
 	
+	private BufferedImage image;
 	public BufferedImage getImage(String path) {
          try {
         	 image = ImageIO.read(getClass().getResourceAsStream(path));
     	} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Failed to get image: " + e.getMessage());
 		}
         return image;
 	}
 	public static String getSoundFile(String path) {
-	     return path;
+		 return path;
 	}
 }
