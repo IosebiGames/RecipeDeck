f78227e280afad2cc5146a752912f36e4022411a
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-06-12 11:06:08 +0400

    Code Changes

--- RecipeManager/Tools/Fonts.java
+++ RecipeManager/Tools/Fonts.java
@@ -11,19 +11,17 @@ public class Fonts {
 	private File fontFile;
 	private GraphicsEnvironment ge;
    	private Font newFont;
-   	private boolean status;
    	
 	public Font AllowExternalFont(String fontPath) {
         try {
         	fontFile = new File(fontPath);
         	fontInput = new FileInputStream(fontFile);
-        	newFont = Font.createFont(Font.TRUETYPE_FONT, fontInput);
         	ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        	newFont = Font.createFont(Font.TRUETYPE_FONT, fontInput);
+        	ge.registerFont(newFont);
         }catch(Exception e) {
         	System.out.println("Failed to setup: " + e.getMessage());
         }
-        status = ge.registerFont(newFont);
-        //System.out.println("Registered: " + status);
         return newFont;
 	}
 }
