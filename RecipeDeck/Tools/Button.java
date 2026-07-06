27b66fc23f9b535326a4972dec076f5e41157d2b
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-07-06 14:39:33 +0400

    Removing soundless button method + Code Changes

--- RecipeDeck/Tools/Button.java
+++ RecipeDeck/Tools/Button.java
@@ -8,7 +8,7 @@ import sound.Sound;
 public class Button {
 	private Fonts f;
 
-	public void createButton(JButton b, Rectangle rect, boolean focusableStatus, JPanel panel, boolean visible, Color bc, Color fc) {
+	public void createButton(JButton b, Rectangle rect, boolean focusableStatus, JPanel panel, boolean visible, Color bc, Color fc, boolean soundless) {
         f = new Fonts();
         f.AllowExternalFont("src/fonts/Inter_bold.ttf");
         b.setFocusable(focusableStatus);
@@ -24,25 +24,16 @@ public class Button {
         for (ActionListener al : b.getActionListeners()) {
             b.removeActionListener(al);
         }
-        b.addActionListener(e -> {
-        	if(e.getSource() == b) {
-        		new Sound().playSound("/sound/click_sound.wav");
-        	}
-	    });
         panel.add(b);
-	}
-	public void createSoundlessButton(JButton b, Rectangle rect, boolean focusableStatus, JPanel panel, boolean visible, Color bc, Color fc) {
-		f = new Fonts();
-        f.AllowExternalFont("src/fonts/Inter_bold.ttf");
-		b.setFocusable(focusableStatus);
-        b.setBounds(rect.getBounds());
-        b.setFont(new Font("Inter", Font.BOLD, 12));
-        b.setBackground(bc);
-        b.setForeground(fc);
-        b.setVisible(visible);
-        b.putClientProperty("JButton.buttonType", "roundRect");
-        b.putClientProperty("FlatLaf.background", Color.white);
-        b.putClientProperty("FlatLaf.foreground", Color.black);
-        panel.add(b);
+        
+        if(soundless == true) {
+        	return;
+        }else {
+        	b.addActionListener(e -> {
+        		if(e.getSource() == b) {
+        			new Sound().playSound("/sound/click_sound.wav");
+        		}
+        	});
+        }
 	}
 }
