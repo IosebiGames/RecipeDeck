9403c712c3a798664652ee4823e871e05493ffe1
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-07-06 14:07:20 +0400

    Code Changes

--- RecipeDeck/Tools/Labels.java
+++ RecipeDeck/Tools/Labels.java
@@ -7,17 +7,12 @@ import java.awt.Font;
 import java.awt.Rectangle;
 
 public class Labels {
-	private Screen screen;
-
-	public Labels(Screen s) {
-		this.screen = s;
-	}
 	public void createLabel(JLabel label, Font font, Color foregroundColor, Rectangle rect, boolean focusableStatus, JPanel partPanel) {
 		label.setBounds(rect.getBounds());
 		label.setFocusable(focusableStatus);
 		label.setForeground(foregroundColor);
 		label.setFont(font);
-		this.screen.window.add(label);
+		Screen.window.add(label);
 		if (partPanel != null) {
 			partPanel.add(label);
 		}
