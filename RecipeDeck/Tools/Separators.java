026d594c3e97b9889b4ef37d8fa2a1e7300dd3f2
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-07-06 14:06:32 +0400

    Code Changes

--- RecipeDeck/Tools/Separators.java
+++ RecipeDeck/Tools/Separators.java
@@ -4,15 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Separators {
-	private Screen screen;
-	
-	public Separators(Screen s) {
-		this.screen = s;
-	}
 	public void createSeparator(JLabel Seperatorlabel, Rectangle rect, boolean opaqueStatus) {
 		Seperatorlabel.setBounds(rect.getBounds());
 		Seperatorlabel.setIcon(new ImageIcon(getClass().getResource("/images/seperator_dark_icon.png")));
 		Seperatorlabel.setOpaque(opaqueStatus);
-		screen.window.add(Seperatorlabel);
+		Screen.window.add(Seperatorlabel);
 	}
 }
