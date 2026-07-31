9051f167a52d60cf4dc1cb686ba460ebfd374c1d
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-07-31 15:11:43 +0400

    Payment value + Target Language

--- RecipeDeck/recipeSystem/OutputManager.java
+++ RecipeDeck/recipeSystem/OutputManager.java
@@ -4,14 +4,39 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import Tools.Language;
 
 public class OutputManager {
-	public static void write(String name, String content) {
-		try (BufferedWriter writer = Files.newBufferedWriter(Path.of(name))) {
-		      writer.write(content);
-		      writer.close();
+	public static void write(String path, String content) {
+		try (BufferedWriter writer = Files.newBufferedWriter(Path.of(path))) {
+              if(Language.currentLanguage.equals("Armenian")) {
+            	  writer.write(content);
+            	  writer.newLine();
+            	  writer.write("Armenian");
+              }else if(Language.currentLanguage.equals("English")) {
+            	  writer.write(content);
+                  writer.newLine();
+            	  writer.write("English");
+              }
+		   writer.close();
 		}catch(IOException e) {
-			System.out.println("Can't write " + name + ": " + e.getMessage());
+			System.out.println("Can't write " + path + ": " + e.getMessage());
 		}
 	}
+	public static void write(String path, String content, boolean Armenian) {
+		try (BufferedWriter writer = Files.newBufferedWriter(Path.of(path))) {
+               if(Armenian) {
+            	   writer.write(content);
+            	   writer.newLine();
+            	   writer.write("Armenian");
+               }else {
+            	   writer.write(content);
+            	   writer.newLine();
+            	   writer.write("English");
+               }
+            writer.close();
+		}catch(IOException e) {
+			System.out.println("Can't write " + path + ": " + e.getMessage());
+		}
+	}	
 }
