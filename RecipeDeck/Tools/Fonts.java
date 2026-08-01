526914282fe30b58b07ade52f1c290cabd8644ac
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-08-01 12:13:24 +0400

    external font loading bug Fixed completely.

--- RecipeDeck/Tools/Fonts.java
+++ RecipeDeck/Tools/Fonts.java
@@ -1,26 +1,25 @@
 package Tools;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 
 public class Fonts {
-	private static InputStream fontInput;
-	private static File fontFile;
 	private static GraphicsEnvironment ge;
    	private static Font newFont;
    	
 	public static Font AllowExternalFont(String fontPath) {
-        try {
-        	fontFile = new File(fontPath);
-        	fontInput = new FileInputStream(fontFile);
+        try (InputStream fontInput = new BufferedInputStream(Fonts.class.getResourceAsStream(fontPath))) {
         	ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         	newFont = Font.createFont(Font.TRUETYPE_FONT, fontInput);
         	ge.registerFont(newFont);
         }catch(Exception e) {
-        	System.out.println("Failed to setup: " + e.getMessage());
+        	if(e.getMessage().contains("Stream closed")) {
+        		// Just check to get reed of "Stream Closed" failure;
+        	}else {
+        		System.out.println("Failed to Setup: " + e.getMessage());
+        	}
         }
         return newFont;
 	}
