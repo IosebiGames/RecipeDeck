e88f8242646a21bef4543f761072c27db70d2250
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-09-01 13:43:50 +0400

    Shorter way to write files

--- RecipeDeck/recipeSystem/OutputManager.java
+++ RecipeDeck/recipeSystem/OutputManager.java
@@ -1,51 +1,38 @@
 package recipeSystem;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import Tools.Decorator;
 import Tools.Language;
 
 public class OutputManager {
 	public static void writeUserRecipe(String path, String userRecipe) {
-		try (BufferedWriter writer = Files.newBufferedWriter(Path.of(path))) {
-                writer.write(userRecipe);
-                writer.close();
+		try {
+            Files.writeString(Path.of(path), userRecipe);
 		}catch(IOException e) {
 			System.out.println("Can't write " + path + ": " + e.getMessage());
 		}
 	}
 	public static void write(String path, String content) {
-		try (BufferedWriter writer = Files.newBufferedWriter(Path.of(path))) {
-              if(Language.currentLanguage.equals("Armenian")) {
-            	  writer.write(content);
-            	  writer.newLine();
-            	  writer.write("Armenian");
-              }else if(Language.currentLanguage.equals("English")) {
-            	  writer.write(content);
-                  writer.newLine();
-            	  writer.write("English");
-              }
-		   writer.close();
+		try {
+            if(Language.currentLanguage.equals("Armenian")) {
+            	  Files.writeString(Path.of(path), content + "\nArmenian");
+            }else if(Language.currentLanguage.equals("English")) {
+            	  Files.writeString(Path.of(path), content + "\nEnglish");
+            }
 		}catch(IOException e) {
 			System.out.println("Can't write " + path + ": " + e.getMessage());
 		}
 	}
 	public static void write(String path, String content, boolean Armenian) {
-		try (BufferedWriter writer = Files.newBufferedWriter(Path.of(path))) {
+		 try {
                if(Armenian) {
-            	   writer.write(content);
-            	   writer.newLine();
-            	   writer.write("Armenian");
+               	  Files.writeString(Path.of(path), content + "\nArmenian");
                }else {
-            	   writer.write(content);
-            	   writer.newLine();
-            	   writer.write("English");
+               	  Files.writeString(Path.of(path), content + "\nEnglish");
                }
-            writer.close();
-		}catch(IOException e) {
+    	}catch(IOException e) {
 			System.out.println("Can't write " + path + ": " + e.getMessage());
 		}
 	}
