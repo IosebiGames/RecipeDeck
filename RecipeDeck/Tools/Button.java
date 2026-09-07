1470074a9a74eee897e25b2e347ddc00ac140690
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-09-07 14:18:03 +0400

    Removed button sound from this class

--- RecipeDeck/Tools/Button.java
+++ RecipeDeck/Tools/Button.java
@@ -3,24 +3,13 @@ package Tools;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import sound.Sound;
-import startup.StartupScreen;
 
 public class Button {
-	private Sound s;
-	
-	public Button() {
-		s = new Sound();
-	}
-	public void createButton(JButton b, Rectangle rect, boolean focusableStatus, JPanel panel, boolean visible, Color bc, Color fc, boolean soundless) {
+	public static void createButton(JButton b, Rectangle rect, boolean focusableStatus, JPanel panel, boolean visible, Color bc, Color fc) {
         b.setFocusable(focusableStatus);
         b.setBounds(rect.getBounds());
         Fonts.AllowExternalFont("RecipeDeck/src/fonts/Inter_bold.ttf");
-        if(StartupScreen.currentLanguage.equals("English")) {
-        	b.setFont(new Font("Inter", Font.BOLD, 12));
-        }else if(StartupScreen.currentLanguage.equals("Armenian")) {
-        	b.setFont(new Font("Verdana", Font.BOLD, 11));
-        }
+        b.setFont(new Font("Inter", Font.BOLD, 12));
         b.setBackground(bc);
         b.setForeground(fc);
         b.setVisible(visible);
@@ -28,22 +17,13 @@ public class Button {
         b.putClientProperty("FlatLaf.background", Color.white);
         b.putClientProperty("FlatLaf.foreground", Color.black);
         
-        for (ActionListener al : b.getActionListeners()) {
+        for(ActionListener al : b.getActionListeners()) {
             b.removeActionListener(al);
         }
-        if(soundless == true) {
-        	return;
+        if(panel == null) {
+            Screen.window.add(b);
         }else {
-        	b.addActionListener(e -> {
-        		if(e.getSource() == b) {
-        			s.playSound("/sound/click_sound.wav");
-        		}
-        	});
-        }
-        if(panel != null) {
-        	panel.add(b);
-        }else {
-        	Screen.window.add(b);
-        }
-	}
-}
+            panel.add(b);
+       }
+ 	}
+ }
