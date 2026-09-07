f6ec5174c5a821443161e6280aca61f7f3f9d00e
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-09-07 14:21:50 +0400

    Removed old methods

--- RecipeDeck/recipeSystem/OutputManager.java
+++ RecipeDeck/recipeSystem/OutputManager.java
@@ -3,8 +3,6 @@ package recipeSystem;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import Tools.Decorator;
-import Tools.Language;
 
 public class OutputManager {
 	public static void writeUserRecipe(String path, String userRecipe) {
@@ -16,31 +14,9 @@ public class OutputManager {
 	}
 	public static void write(String path, String content) {
 		try {
-            if(Language.currentLanguage.equals("Armenian")) {
-            	  Files.writeString(Path.of(path), content + "\nArmenian");
-            }else if(Language.currentLanguage.equals("English")) {
-            	  Files.writeString(Path.of(path), content + "\nEnglish");
-            }
-		}catch(IOException e) {
+             Files.writeString(Path.of(path), content);
+       }catch(IOException e) {
 			System.out.println("Can't write " + path + ": " + e.getMessage());
 		}
-	}
-	public static void write(String path, String content, boolean Armenian) {
-		 try {
-               if(Armenian) {
-               	  Files.writeString(Path.of(path), content + "\nArmenian");
-               }else {
-               	  Files.writeString(Path.of(path), content + "\nEnglish");
-               }
-    	}catch(IOException e) {
-			System.out.println("Can't write " + path + ": " + e.getMessage());
-		}
-	}
-	public static void translate(boolean Armenian) {
-		if(Armenian) {
-		   write("src/output/payment.txt", String.valueOf(Decorator.addings), true);	
-		}else {
-           write("src/output/payment.txt", String.valueOf(Decorator.addings), false);
-	   }  
 	}
 }
